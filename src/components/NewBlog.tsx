0ba0e3b344b03ffc3cdfe9a5d8112c7231659bbb
Reinald Reynoso <reireynoso@gmail.com>
2020-12-30 15:50:12 -0500

    select cohort and tag select feature implemented

--- src/components/NewBlog.tsx
+++ src/components/NewBlog.tsx
@@ -1,14 +1,24 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useStateValue} from '../context-api/Provider';
 import {server} from '../config/endpoints';
 import Cookies from 'universal-cookie';
 import {setCohorts} from '../context-api/actions';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import acceptableTags from '../config/tags';
 
 const cookies = new Cookies();
 
 const NewBlog : React.FC = () => {
 
     const [{user, cohorts}, dispatch] = useStateValue();
+    const [tags, setTags] = useState<string[]>([])
+    const [link, setLink] = useState<string>("");
+    const [cohort, setCohort] = useState<string>("")
 
     useEffect(() => {
         // auto fetch cohorts 
@@ -23,30 +33,107 @@ const NewBlog : React.FC = () => {
         }
     }, [cohorts])
 
+    const handleLinkChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setLink(e.target.value);
+    }
+
+    const handleCohortChange = (e:React.ChangeEvent<{value:unknown}>) => {
+        // console.log(e.target.value as string)
+        setCohort(e.target.value as string)
+    }
+
+    const handleSubmit = ():void => {
+        const blogInformation = {
+            link,
+            cohort,
+            tags
+        }
+        fetch(`${server}/blog/new`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application.json",
+                "Authorization": `Bearer ${cookies.get("token")}`
+            },
+            body: JSON.stringify(blogInformation)
+        })
+        .then(res => res.json())
+        .then(console.log)
+    }
+
     return (
         <div>
             {
                 !user ?  
                 "Please Login first before posting a blog" 
                 :
-                <form>
-                    <label>Title</label>
-                    <input type="text" placeholder="Title" name="title"/>
-                    <label>Link</label>
-                    <input type="text" placeholder="Link" name="link"/>
-                    <label>Cohort</label>
-                    <select>
-                        {
-                            cohorts.map((cohort:{name:string}) => (
-                                <option>{cohort.name}</option>
-                            ))
-                        }
-                    </select>
-                    <input type="submit" value="Post Blog"/>
-                </form>
+                <div>
+                    <TextField
+                        variant="standard"
+                        label="Link URL"
+                        placeholder="Medium Blog"
+                        error={tags.length > 5}
+                        helperText="Please provide a friend link"
+                        onChange={handleLinkChange}
+                        value={link}
+                    />
+
+                    <FormControl>
+                        <InputLabel id="demo-simple-select-label">Cohort</InputLabel>
+                        <Select
+                        labelId="demo-simple-select-label"
+                        value={cohort}
+                        onChange={handleCohortChange}
+                        >
+                            {
+                                cohorts.map((cohort:{name:string}) => (
+                                    <MenuItem 
+                                        key={cohort.name}
+                                        value={cohort.name}
+                                        >
+                                            {cohort.name}
+                                        </MenuItem>
+                                ))
+                            }
+                        </Select>
+                    </FormControl>
+                    
+                    <Autocomplete
+                        multiple
+                        id="tags-standard"
+                        limitTags={3}
+                        options={acceptableTags}
+                        // getOptionLabel={(option) => option.name}
+                        renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            variant="standard"
+                            label="Tags"
+                            placeholder="Select tags"
+                            error={tags.length > 5}
+                            helperText="5 Tags Max!"
+                        />
+                        )}
+                        // getOptionSelected={(option, value) => {
+                        //     console.log(option.name)
+                        //     return option.name === value.name
+                        // }}
+                        // onInputChange={(val) => console.log('change', val)}
+                        onChange={(e, val) => {
+                            // on select, the function manages adding the selected value to the array value and set the tags.
+                                setTags(val);
+                        }}
+                        value={tags}
+                    />
+
+                    <button onClick={handleSubmit}>
+                        Post blog
+                    </button>
+            </div>
 
             }
         </div>
+        
     )
 }
 
