880548f14612646574734e666037c7f08876d9aa
Reinald Reynoso <reireynoso@gmail.com>
2021-02-08 21:48:35 -0500

    updated endpoints

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,4 +1,4 @@
 module.exports = {
-    server: process.env.NODE_ENV === "production" ? null : "http://localhost:4000",
-    client: process.env.NODE_ENV === "production" ? null : "http://localhost:3000"
+    server: process.env.NODE_ENV === "production" ? "https://coding-blogs-api.herokuapp.com" : "http://localhost:4000",
+    client: process.env.NODE_ENV === "production" ? "https://fis-blogs-client.web.app" : "http://localhost:3000"
 }
